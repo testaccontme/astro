175611be3581c946e6afb0849c122986339ab2e9
pooja <raunixdatateam17@gmail.com>
2025-03-04 18:22:52 +0530

    first commit

---
+++ lib/utils/config.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: non_constant_identifier_names, dangling_library_doc_comments
+
+///  =================================================================
+/// ********************** BASE URL FOR API ********************
+/// ==================================================================
+///
+String contactsupportEmail = "support@gmail.com";
+String imgBaseurl = "https://vedicbhagya.in/";
+String pdfBaseurl = "https://vedicbhagya.in/public";
+String appMode = "LIVE";
+String OtplessappId = "A57OBN9737HZYKHXRGK2";
+
+String storiesIcon = "assets/images/stories_icon.png";
+
+Map<String, dynamic> appParameters = {
+  "LIVE": {
+    "apiUrl": "https://vedicbhagya.in/api/",
+  },
+  "DEV": {
+    "apiUrl": "https://vedicbhagya.in/api/",
+  }
+};
